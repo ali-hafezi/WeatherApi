9d0ba975ec0d20c324029af44b289a878f9f9dfb
ali-hafezi <hafezi7@yahoo.com>
2025-11-15 20:14:04 +0330

    .net 10

--- WeatherApi.RestApi/Common/BaseController.cs
+++ WeatherApi.RestApi/Common/BaseController.cs
@@ -2,7 +2,7 @@
 
 namespace WeatherApi.RestApi.Common;
 
-public class BaseController:ControllerBase
+public class BaseController : ControllerBase
 {
     protected readonly ICommandBus commandBus;
     protected readonly IQueryBus queryBus;
